4d684bcfd8c368fb473a0b2dcbd5c6e1b7aa3718
ccianos <ccianos@alphavida.com>
2020-09-06 22:58:56 -0700

    Working with lists.

--- variables_collections.py
+++ variables_collections.py
@@ -21,3 +21,20 @@ some_var # => 5
 
 # if can be used as an ternary-like expression 
 "Woo!" if 0 > 1 else "Arrr!" # => 'Arrr!'
+
+# Lists store sequences and can be prefilled during assigment.
+li = []
+other_li = [4, 5, 6]
+
+# Add to the list via the append method.
+li.append(1) # => li is now [1]
+li.append(2) # => li is now [1, 2]
+li.append(4) # => li is now [1, 2, 4]
+li.append(3) # => li is now [1, 2, 4, 3]
+# Remove an element from the list's end with pop by default,
+# or at specified index.
+li.pop() # => li is now [1, 2, 4]
+
+li.append(3) # append 3 back
+li # => [1, 2, 4, 3]
+
