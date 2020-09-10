8d5bd0f302345feeaa3ffbb51af71fada75b799d
ccianos <ccianos@alphavida.com>
2020-09-10 00:51:07 -0700

    if statements.

---
+++ control_flow_and_iterables.py
@@ -0,0 +1,14 @@
+# A variable
+some_var = 5
+
+# if statement demonstrating convention of indenting with 4 spaces.
+# if statement will output "some_var is smaller than 10"
+if some_var > 10:
+    print("some_var is absolutely bigger than 10.")
+elif some_var < 10:
+    print("some_var is smaller smaller than 10")
+else:
+    print("some_var is 10.")
+
+"""
+"""
